9e4acfc9e6443dd60c40b5f5fe7585bc76092943
Lukas <lukas.tabulevicius@gmail.com>
2022-03-29 13:04:34 +0300

    variables

---
+++ js/variables.js
@@ -0,0 +1,37 @@
+/*
+Kintamieji:
+(Inicijavimo budas)
+const - default budas
+let - antrinis budas, kai zinome, jog reiksme keisis
+var - pats seniausias, geriau nenaudoti, jei rupi saugumas
+
+Duomenu tipoas: 
+number = 1; 1.5; 9999;
+string (tekstas) = abd; Lukas;
+boolean (logine reiksme) = true; false.
+array (sarasas, masyvas, matrica)
+
+*/
+
+const skaicius = 777;
+console.log('Laimingas skaicius yra ' + skaicius);
+
+let darSkaicius = 0;
+console.log(darSkaicius);
+
+darSkaicius = 10;
+
+darSkaicius = 21;
+console.log(darSkaicius);
+
+const priskyrimas = 'Tavo skaicius yra ' + darSkaicius + '!';
+console.log(priskyrimas);
+
+const arLaimingas = false;
+console.log('Ar tavo skaicius laimingas? ' + arLaimingas);
+
+const nelaimingi = [1, 0, 776, 999];
+console.log('Siandien nelaimejo: ' + nelaimingi);
+
+const zaidejai = ['Jonas', 'Jasonas', 'Joana'];
+console.log(zaidejai);
