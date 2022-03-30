80153b2d1b37daddafc55a429f5c0a6e4214de21
Lukas <lukas.tabulevicius@gmail.com>
2022-03-30 13:15:57 +0300

    array ir if

---
+++ js/04-array.js
@@ -0,0 +1,16 @@
+/*
+ARRAY
+pozicija prasideda nuo 0 (array[0] paims pirma elementa)
+
+*/
+
+let pazymiai = [10, 2, 8, 0, 9];
+console.log('Pazymiai:', pazymiai);
+console.log('Pazymiai: ' + pazymiai);
+
+console.log('xxxxxxxxxxxxxxxxxxxx');
+
+console.log('Pirmas:', pazymiai[0]);
+console.log('Antras:', pazymiai[1]);
+console.log('Paskutinis:', pazymiai[pazymiai.length-1]);
+console.log('Pazymiu kiekis', pazymiai.length);
---
+++ js/05-if.js
@@ -0,0 +1,28 @@
+/*
+IF
+
+Sablonai: 
+if () {}
+if () {} else {}
+
+Salyginiai operatoriai:
+visi: >, <, <=, >=, == (reiksme lygu), != (reiksme nelygu); === (reiksme lygu ir tipas sutampa); !== (reiksme nelygu ir tipai sutampa)
+naudotini: >, <, <=, >=, ===, !==
+NEnaudotini: ==, !=
+
+
+*/
+
+let a = 2;
+let b = 1;
+
+if (a == b) {
+    console.log('a is equal to b');
+}  else { console.log('a is NOT equal to b');
+}
+
+if (b != a) {
+    console.log('b is NOT equal to a');
+} else {
+    console.log('b is equal to a');
+}
