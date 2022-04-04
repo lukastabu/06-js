d0437d551b07dd6fb99f5aa448ea1a19e4bd609e
Lukas <lukas.tabulevicius@gmail.com>
2022-04-04 10:42:53 +0300

    loops

--- js/07-for.js
+++ js/07-for.js
@@ -1,7 +1,8 @@
 /*
 LOOP: for, while, for-in, for-of, foreach, etc...
 */
-
+console.log('xxxxxxxxxxxxxxxxxxxxxxxx');
+console.log('FOR CIKLAS');
 let pazymiai = [5, 1, 10, 9];
 let suma = 0
 
@@ -17,3 +18,39 @@ console.log('xxxxxxxxxxxxxxxxxxxxxxxx');
 for (let i= -10; i < 1; i++) {
     console.log(i);
 }
+
+console.log('xxxxxxxxxxxxxxxxxxxxxxxx');
+console.log('WHILE CIKLAS');
+
+let sumaWhile = 0;
+iw = 0;
+while (iw < pazymiai.length) {
+    sumaWhile += pazymiai[iw];
+    console.log();
+}
+
+vidurkis = sumaWhile / pazymiai.length;
+log(vidurkis);
+
+console.log('xxxxxxxxxxxxxxxxxxxxxxxx');
+console.log('FOR-OF ciklas');
+
+let sumaForOf = 0;
+let iFO = 0;
+for (const pazymys of pazymiai) {
+    sumaForOf += pazymys;    
+}
+
+vidurkis = sumaForOf / pazymiai.length;
+console.log(vidurkis);
+
+console.log('xxxxxxxxxxxxxxxxxxxxxxxx');
+console.log('FOREACH ciklas');
+
+let sumaForEach = 0;
+pazymiai.forEach((pazymys, iFE, pazymiaiRef) => {
+    sumaForEach += pazymys;
+});
+
+vidurkis = sumaForEach / pazymiai.length;
+console.log(vidurkis);
